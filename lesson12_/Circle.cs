ec7383529540b5e14cab754380d6f9130f97bcbf
Katerina <iglkotik@gmail.com>
2024-12-02 21:44:12 +0300

    Добавьте файлы проекта.

---
+++ lesson12_/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les12
+{
+    public static class Circle
+    {
+        const double x0 = 0;
+        const double y0 = 1;
+
+        static public double GetLenght(double r)
+        { return Math.PI * r; }
+
+        static public double GetSquare(double r)
+        { return Math.PI * r * r; }
+
+        static public bool GetPrin(double x1, double y1, double r)
+        {
+            return ((x1 - x0) * (x1 - x0)) + ((y1 - y0) * (y1 - y0)) <= r * r;
+
+        }
+
+    }
+}
---
+++ lesson12_/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Circle.GetLenght(r) + " " + Circle.GetSquare(r));
+            Console.WriteLine("Введите точку с координатами x, y");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            if (Circle.GetPrin(x, y, r))
+            {
+                Console.WriteLine("Точка принадлежит окружности");
+            }
+            else
+            {
+                Console.WriteLine("Точка не принадлежит окружности");
+            }
+            Console.ReadKey();
+        }
+    }
+}
